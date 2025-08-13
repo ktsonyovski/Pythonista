979a3de31f24f3b5d8afdddde38e21ac90ab15cd
ktsonyovski <kaloyan.tsonyovski@festo.com>
2025-08-13 09:41:12 +0300

    Refactor is_leap_year function to simplify leap year logic

--- basic_functions/basic_functions/main.py
+++ basic_functions/basic_functions/main.py
@@ -161,10 +161,6 @@ def is_leap_year(year: int) -> bool:
     Returns:
         bool: True if the year is a leap year, False otherwise.
     """
-    if year % 400 == 0:
-        return True
-    if year % 100 == 0:
-        return False
-    if year % 4 == 0:
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
         return True
     return False
